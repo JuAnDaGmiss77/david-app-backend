d92e85ab08ec4f08a0d3ad7c7f2218e2e40a42a6
JuAnDaGmiss77 <juandavidb741@gmail.com>
2021-12-05 22:18:36 -0500

    cors modificado

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ import ipRoutes from './routes/ip';
 
 const app = express();
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'https://david-app-20.herokuapp.com');
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
