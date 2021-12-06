a11b83efc12a1e9db063d829ca0e84abff76cfb2
JuAnDaGmiss77 <juandavidb741@gmail.com>
2021-12-05 22:11:10 -0500

    cors modificado

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,15 @@ import authRoutes from './routes/auth';
 import ipRoutes from './routes/ip';
 
 const app = express();
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 
-app.use(cors());
+// app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 
