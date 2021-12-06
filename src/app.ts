72652f840aa951a9d071ed153bc6aec34bbb6f94
JuAnDaGmiss77 <juandavidb741@gmail.com>
2021-12-05 22:33:06 -0500

    cors

--- src/app.ts
+++ src/app.ts
@@ -5,15 +5,11 @@ import authRoutes from './routes/auth';
 import ipRoutes from './routes/ip';
 
 const app = express();
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://david-app-20.herokuapp.com');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-});
-
-// app.use(cors());
+const corsOptions = {
+    origin: 'https://david-app-20.herokuapp.com',
+    optionsSuccessStatus: 200 // For legacy browser support
+}
+app.use(cors(corsOptions));
 app.use(morgan("dev"));
 app.use(express.json());
 
