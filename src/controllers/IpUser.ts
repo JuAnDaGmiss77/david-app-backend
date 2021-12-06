2ddca64eb69d5c059af3060e88b6a054cb9c7ee5
JuAnDaGmiss77 <juandavidb741@gmail.com>
2021-12-05 23:17:30 -0500

    debug en funcion getIPUser

--- src/controllers/IpUser.ts
+++ src/controllers/IpUser.ts
@@ -1,27 +1,27 @@
-import Ip , {IP} from '../models/IP';
-import {Request, Response} from 'express';
+import Ip, { IP } from "../models/IP";
+import { Request, Response } from "express";
 
-export const savedIPUser = async (req: Request, res: Response ) => {
-    const ipFound = await Ip.findOne({ip: req.body.ip});
-    if (ipFound) return res.status(409).json({ message: "ip is saved"});
+export const savedIPUser = async (req: Request, res: Response) => {
+  const ipFound = await Ip.findOne({ ip: req.body.ip });
+  if (ipFound) return res.status(409).json({ message: "ip is saved" });
 
-    const ip: IP = new Ip({
-        ip: req.body.ip
-    })
-    const ipSaved = await ip.save();
-    res.status(201).json({ message: "ip saved", ipSaved});
-
-}
+  const ip: IP = new Ip({
+    ip: req.body.ip,
+  });
+  const ipSaved = await ip.save();
+  res.status(201).json({ message: "ip saved", ipSaved });
+};
 
 export const getIPUser = async (req: Request, res: Response) => {
-    console.log('entrando al metodo getipuser')
+  try {
+    console.log("entrando al metodo getipuser");
     const ipFound = await Ip.findOne();
-    console.log('esta es la variable ipfound '+ipFound);
-    console.log(ipFound)
-    if (ipFound) return res.status(200).json({ message: "list ip" , ipFound});
+    console.log("esta es la variable ipfound " + ipFound);
+    console.log(ipFound);
+    if (ipFound) return res.status(200).json({ message: "list ip", ipFound });
 
-    res.status(404).json({message: "ip not found"})
-
-    
-
-}
+    res.status(404).json({ message: "ip not found" });
+  } catch (error) {
+      console.error(error);
+  }
+};
